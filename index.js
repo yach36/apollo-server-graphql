ab617a14c760888671c3afce779d9550685ea644
yach36 <yach0306@gmail.com>
2023-05-20 03:49:13 +0900

    feat: userに関連するpostを取得

--- index.js
+++ index.js
@@ -6,12 +6,21 @@ const typeDefs = gql`
     id:  ID!
     name:  String!
     email: String!
+    myPosts: [Post]
+  }
+
+  type Post {
+    id: ID!
+    title: String!
+    body: String!
+    userId: ID!
   }
 
   type Query {
     hello(name: String!): String
     users: [User]
     user(id: ID!): User
+    posts: [Post]
   }
 `
 
@@ -30,11 +39,26 @@ const resolvers = {
       return response.data
     },
     user: async (parent, args) => {
-      const response = await axios.get(
+      let response = await axios.get(
         `https://jsonplaceholder.typicode.com/users/${args.id}`
       )
-      return response.data
+      let user = response.data
+
+      response = await axios.get(
+        'https://jsonplaceholder.typicode.com/posts'
+      )
+      const myPosts = response.data.filter((post) => post.userId === user.id)
+      user = Object.assign({}, user, {
+        myPosts: myPosts,
+      })
+      return user
     },
+    posts: async () => {
+      const response = await axios.get(
+        'https://jsonplaceholder.typicode.com/posts'
+      )
+      return response.data
+    }
   }
 }
 
